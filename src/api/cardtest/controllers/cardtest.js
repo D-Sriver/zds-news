1a6dbf7878fa3dab61410d7b1fd5f452bfbbbca3
D.Sriver <duez.s@proton.me>
2024-09-11 21:49:36 +0200

    🚀 Initial commit

---
+++ src/api/cardtest/controllers/cardtest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cardtest controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cardtest.cardtest');
---
+++ src/api/cardtest/routes/cardtest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cardtest router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cardtest.cardtest');
